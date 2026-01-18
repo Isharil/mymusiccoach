41bbe72727fbbf23b1f0cc6e985fbcc95c06f45a
Isharil <metalicsound13@gmail.com>
2026-01-18 12:31:26 +0100

    Remplacement de l'icone Poubelle dans 'Mes Exercices' par une icône Archive

--- src/MyMusicCoach.jsx
+++ src/MyMusicCoach.jsx
@@ -1896,9 +1896,9 @@ const MyMusicCoach = () => {
                   onClick={() => setShowTrash(true)}
                   className="bg-gray-100 text-gray-700 p-3 rounded-full shadow hover:bg-gray-200 transition-colors relative"
                 >
-                  <Trash2 className="w-5 h-5" />
+                  <Archive className="w-5 h-5" />
                   {deletedExercises.length > 0 && (
-                    <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
+                    <span className="absolute -top-1 -right-1 bg-orange-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
                       {deletedExercises.length}
                     </span>
                   )}
